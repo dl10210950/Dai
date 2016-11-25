bb31fce1655931ee183e2a5be3e981ddcda3fb3a
duanlian <10210950@qq.com>
2016-11-25 18:14:05 +0800

    加入搜索箭头

---
+++ app/src/main/java/com/duanlian/daimeng/constant/Api.java
@@ -0,0 +1,17 @@
+package com.duanlian.daimeng.constant;
+
+/**
+ * 放置接口地址的
+ */
+
+public class Api {
+    /**
+     * 百度api store apikey
+     */
+    public static final String baiduApiKey = "273a9e54fa6cd402b332b723d54ac6f5";
+
+    /**
+     * 频道新闻API_易源
+     */
+    public static final String yiYuanNews = "http://apis.baidu.com/showapi_open_bus/channel_news/search_news";
+}
--- app/src/main/java/com/duanlian/daimeng/ui/fragment/NewsFragment.java
+++ app/src/main/java/com/duanlian/daimeng/ui/fragment/NewsFragment.java
@@ -4,19 +4,50 @@ import android.view.View;
 
 import com.duanlian.daimeng.R;
 import com.duanlian.daimeng.base.BaseFragment;
+import com.duanlian.daimeng.utils.LogUtils;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import okhttp3.Call;
+
+import static com.duanlian.daimeng.constant.Api.baiduApiKey;
+import static com.duanlian.daimeng.constant.Api.yiYuanNews;
 
 /**
  * music页面
  */
 
-public class NewsFragment extends BaseFragment{
+public class NewsFragment extends BaseFragment {
     public static BaseFragment newInstance() {
         NewsFragment fragment = new NewsFragment();
         return fragment;
     }
+
     @Override
     public View initView() {
         View view = View.inflate(getBaseActivity(), R.layout.fragment_news, null);
+        view.findViewById(R.id.news_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OkHttpUtils.get()
+                        .url(yiYuanNews)
+                        .addHeader("apikey", baiduApiKey)
+                        .build()
+                        .execute(new StringCallback() {
+                            @Override
+                            public void onError(Call call, Exception e, int id) {
+                                LogUtils.e(getBaseActivity(), e.toString());
+                            }
+
+                            @Override
+                            public void onResponse(String response, int id) {
+                                LogUtils.e(getBaseActivity(), response.toString());
+                            }
+                        });
+
+
+            }
+        });
         return view;
     }
 }
